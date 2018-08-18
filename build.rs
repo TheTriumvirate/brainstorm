e27742092695afd73a4f760825722371961dc447
theAzack9 <robin@cavachi.be>
2018-08-18 15:11:53 +0200

    Undo breaking changes to bindings

--- build.rs
+++ build.rs
@@ -1,16 +1,10 @@
-#[cfg(target_arch = "wasm32")]
 extern crate webgl_generator;
 
-#[cfg(target_arch = "wasm32")]
 use std::env;
-#[cfg(target_arch = "wasm32")]
 use std::fs::File;
-#[cfg(target_arch = "wasm32")]
 use std::path::*;
-#[cfg(target_arch = "wasm32")]
 use webgl_generator::*;
 
-#[cfg(target_arch = "wasm32")]
 fn main() {
     let dest = env::var("OUT_DIR").unwrap();
     let mut file = File::create(&Path::new(&dest).join("webgl_rendering_context.rs")).unwrap();
@@ -20,5 +14,3 @@ fn main() {
         .unwrap();
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-fn main() { }
