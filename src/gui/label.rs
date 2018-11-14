281f0644e725a830f85caad07cd0cfec5e7b13fd
Stian Soltvedt <stian@soltvedt.info>
2018-11-14 13:45:59 +0100

    Add debug visualization to labels

--- src/gui/label.rs
+++ src/gui/label.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use graphics::{position, Drawable, Font, Text};
+use graphics::{position, Drawable, Font, Text, Rectangle};
 use gui::UiElement;
 use na::Matrix4;
 
@@ -8,6 +8,7 @@ use na::Matrix4;
 pub struct Label<'a> {
     pos: position::Absolute,
     text: Text<'a>,
+    rect: Rectangle,
 }
 
 impl<'a> Label<'a> {
@@ -19,10 +20,13 @@ impl<'a> Label<'a> {
         font: Rc<RefCell<Font<'a>>>,
     ) -> Self {
         let coords = pos.to_relative(screensize).get_coordinates();
-
+        let mut rect_coord = pos.clone();
+        rect_coord.width = 5;
+        rect_coord.height = 5;
         Self {
             pos,
             text: Text::new(text, font, coords.x1, coords.y1, screensize),
+            rect: Rectangle::new(rect_coord.to_relative(screensize).get_coordinates(), (1.0, 1.0, 1.0)),
         }
     }
 
@@ -35,11 +39,17 @@ impl<'a> UiElement for Label<'a> {
     fn resize(&mut self, screensize: (f32, f32)) {
         let text_coords = self.pos.to_relative(screensize).get_coordinates();
         self.text.set_position(text_coords.x1, text_coords.y1, screensize);
+
+        let mut rect_coord = self.pos.clone();
+        rect_coord.width = 5;
+        rect_coord.height = 5;
+        self.rect = Rectangle::new(rect_coord.to_relative(screensize).get_coordinates(), (1.0, 1.0, 1.0));
     }
 }
 
 impl<'a> Drawable for Label<'a> {
     fn draw_transformed(&self, view_matrix: &Matrix4<f32>) {
+        self.rect.draw_transformed(view_matrix);
         self.text.draw_transformed(view_matrix);
     }
 }
